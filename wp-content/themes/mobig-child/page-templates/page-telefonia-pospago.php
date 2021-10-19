157f56a40e4790bb04ab72fd4a22363a23836e69
Leslye Pérez <leslye@concepthaus.mx>
2021-10-19 08:59:23 -0500

    se sube pantalle de telefonia pos pago

---
+++ wp-content/themes/mobig-child/page-templates/page-telefonia-pospago.php
@@ -0,0 +1,197 @@
+<?php
+/**
+ * Template name: Telefonia-Pospago
+ *
+ * @package moBig_Child
+ * 
+ */
+
+ get_header();
+?>
+
+  <article class="bg-gen">
+   
+    <section class="hero-sec">
+      <div class="container">
+        <div class="row">
+            <img src=""<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/Iconos o moBig.svg" class="w-50" alt="logoMoBig" />
+            <h1>POSPAGO</h1>
+            <p>Una vez realizado el proceso de portabilidad elige uno de nuestros planes de pospago que se adapte mejor a tus necesidades y determina la tarjet de crédito o débito al que se realizará el cargo mensual..</p>
+            <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/telefonia-pospago/header.png" alt="header-section" class="w-50" />
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.hero-sec -->
+   
+
+    <section class="zonawifi-sec">
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <div class="col-sm-10">
+            <div class="row">
+              <h1>PLANES POSPAGO</h1>
+              <p>Sin importar la forma de apgo que eligas, ten la seguridad de que siempre podrás seguir navegando y disfrutar de tus redes sociales favoritas, mientras te mantienes en contacto con tus seres queridos.</p>
+            </div>
+            <div class="row">
+              <div class="card col-sm-3" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN COMPARTE</h3>
+                  <h4>5GB</h4>
+                  <h1>$115</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>5 GB de navegación (datos para compartir)</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              
+              <div class="card col-sm-3" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN INDIVIDUAL</h3>
+                  <h4>20GB</h4>
+                  <h1>$220</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>20 GB de navegación +20 GB a velocidad reducida</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div class="card col-sm-3" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN COMPARTE</h3>
+                  <h4>20GB</h4>
+                  <h1>$330</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>20 GB de navegación +20 GB a velocidad reducida</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Datos para compartir</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div class="card col-sm-3" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN COMPARTE</h3>
+                  <h4>Ilimitado</h4>
+                  <h1>$115</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>50 GB de navegación (datos para compartir)</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.zonawifi-sec -->
+
+     <section class="cobertura-sec">
+      <div class="container">
+        <div class="row">
+          <h1>PLANES ANUALIZADOS, PAGA 7 MESES Y LLÉVATE 12</h1>
+        </div>
+        <div class="row">         
+          <div class="card col-sm-3" >
+            <div class="card-body">
+              <h3 class="card-title text-center">PLAN INDIVIDUAL</h3>
+              <h4>5GB</h4>
+              <h1>$700</h1>
+              <ul>
+                <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Costo mensual $100 pesos paga 7 y obtén 12 meses</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>5GB de navegación (Datos por compartir)</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+              </ul>
+              <a href="#" class="btn btn-primary">Comprar</a>
+            </div>
+          </div>
+          <div class="card col-sm-3" >
+            <div class="card-body">
+              <h3 class="card-title text-center">PLAN INDIVIDUAL</h3>
+              <h4>5GB</h4>
+              <h1>$1400</h1>
+              <ul>
+                <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Costo mensual $200 pesos paga 7 y obtén 12 meses</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>20GB de navegación +20 GB a velocidad reducida</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+              </ul>
+              <a href="#" class="btn btn-primary">Comprar</a>
+            </div>
+          </div>
+          <div class="card col-sm-3" >
+            <div class="card-body">
+              <h3 class="card-title text-center">PLAN COMPARTE</h3>
+              <h4>20GB</h4>
+              <h1>$2100</h1>
+              <ul>
+                <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Costo mensual $300 pesos paga 7 y obtén 12 meses</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>20GB de navegación +20 GB a velocidad  reducida (Datos por compartir)</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Llamadas Y SMS ilimitads a México, EU y Canadá</p></li>
+                <li><i class="fa fa-check-circle"></i> <p>Redes sociales ilimitadas</p></li>
+              </ul>
+              <a href="#" class="btn btn-primary">Comprar</a>
+            </div>
+          </div>
+          <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/telefonia-pospago/4571203.png" class="w-50" alt="telefonia-pospago" />
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.cobertura-sec -->
+
+    <section class="beneficios-sec">
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <div class="col-sm-10">
+            <div class="row">
+              <h1>¿Dudas? Por acá hay algunas preguntas frecuentes</h1>
+            </div>
+            <div class="row d-flex justify-content-center">
+              <?php if( have_rows('faqs_tel_pos') ):
+              $row = 0;
+              ?>
+                <div class="col-sm-10">
+                <div class="accordion" id="accordionExample">
+                <?php while( have_rows('faqs_tel_pos') ): the_row();
+                    ?>
+
+                    <div class="accordion-item ">
+                      <h2 class="accordion-header" id="FAQ-<?php echo $row; ?>">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFAQ-<?php echo $row; ?>" aria-expanded="false" aria-controls="collapseOne">
+                          <?php the_sub_field('pregunta_telpos_field'); ?>
+                        </button>
+                      </h2>
+                      <div id="collapseFAQ-<?php echo $row; ?>" class="accordion-collapse collapse" aria-labelledby="FAQ-<?php echo $row; ?>" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <p><?php the_sub_field('respuesta_telpos_field'); ?></p>
+                        </div>
+                      </div>
+                    </div>
+                <?php 
+                  $row++;
+              endwhile; ?>
+                  </div>
+</div>
+            <?php endif; ?>
+            </div>
+          </div>
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.beneficios-sec -->
+  </article><!-- end.article inicio -->
+
+<?php
+get_footer();
---
+++ wp-content/themes/mobig-child/page-templates/page-telefonia-prepago.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * Template name: Telefonia-Prepago
+ *
+ * @package moBig_Child
+ * 
+ */
+
+ get_header();
+?>
+
+  <article class="bg-gen">
+   
+    <section class="hero-sec">
+      <div class="container">
+        <div class="row">
+            <img src=""<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/Iconos o moBig.svg" class="w-50" alt="logoMoBig" />
+            <h1>TEN LA SEGURIDAD QUE SIEMPRE ESTARAS CONECTADO</h1>
+            <p>moBig Mifi es tu propia zona Wifi, sin importar donde vayas el internet te acompaña.</p>
+            <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/mifi/4571203.png" alt="header-section" class="w-50" />
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.hero-sec -->
+
+    <section class="cobertura-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/mifi/mifi.png" class="w-50" alt="mifi" />
+          </div>
+          <div class="col-sm-6">
+            <h1>MIFI ES NUESTRO MODEM INALÁMBRICO</h1>
+            <p>Su práctico tamaño te permite transportarlo a todos lados las 24 horas del día con cobertura nacional.</p>
+          </div>
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.cobertura-sec -->
+
+    <section class="zonawifi-sec">
+      <div class="container">
+        <div class="row d-flex justify-content-center">
+          <div class="col-sm-10">
+            <div class="row">
+              <h1>APROVECHA NUESTROS PLANES PREPAGO</h1>
+              <p>Sin importar donde vayas con moBig Mifi tu internet con tus amigos y clientes en tu casa, establecimiento, oficina o en cualquier parte.</p>
+            </div>
+            <div class="row">
+              <div class="card col-sm-4" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN MIFI 1</h3>
+                  <h4>5GB</h4>
+                  <h1>$100</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>5 GB de navegación en territorio nacional</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div class="card col-sm-4" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN MIFI 2</h3>
+                  <h4>10GB</h4>
+                  <h1>$175</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>10 GB de navegación en territorio nacional</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div class="card col-sm-4" >
+                <div class="card-body text-center">
+                  <h3 class="card-title">PLAN MIFI 3</h3>
+                  <h4>20GB</h4>
+                  <h1>$300</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>20 GB de navegación en territorio nacional</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div class="card col-sm-4" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN MIFI 4</h3>
+                  <h4>50GB</h4>
+                  <h1>$500</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>50 GB de navegación en territorio nacional</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+              <div class="card col-sm-4" >
+                <div class="card-body">
+                  <h3 class="card-title text-center">PLAN MIFI 5</h3>
+                  <h4>100GB</h4>
+                  <h1>$1000</h1>
+                  <ul>
+                    <li><i class="fa fa-check-circle"></i> <p>30 días de vigencia</p></li>
+                    <li><i class="fa fa-check-circle"></i> <p>100 GB de navegación en territorio nacional</p></li>
+                  </ul>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.zonawifi-sec -->
+
+    <section class="beneficios-sec">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-10">
+            <div class="row">
+              <h1>¿Dudas? Por acá hay algunas preguntas frecuentes</h1>
+            </div>
+            <div class="row">
+              <?php if( have_rows('faqs_mifi') ):
+              $row = 0;
+              ?>
+                <div class="col-sm-10">
+                <div class="accordion" id="accordionExample">
+                <?php while( have_rows('faqs_mifi') ): the_row();
+                    ?>
+
+                    <div class="accordion-item col-sm-6">
+                      <h2 class="accordion-header" id="FAQ-<?php echo $row; ?>">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFAQ-<?php echo $row; ?>" aria-expanded="false" aria-controls="collapseOne">
+                          <?php the_sub_field('pregunta_field'); ?>
+                        </button>
+                      </h2>
+                      <div id="collapseFAQ-<?php echo $row; ?>" class="accordion-collapse collapse" aria-labelledby="FAQ-<?php echo $row; ?>" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                          <p><?php the_sub_field('respuesta_field'); ?></p>
+                        </div>
+                      </div>
+                    </div>
+                <?php 
+                  $row++;
+              endwhile; ?>
+                  </div>
+</div>
+            <?php endif; ?>
+            </div>
+          </div>
+        </div><!-- end.row -->
+      </div><!-- end.container -->
+    </section><!-- end.beneficios-sec -->
+  </article><!-- end.article inicio -->
+
+<?php
+get_footer();
