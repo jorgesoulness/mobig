8c17bcf8714c23fd20ee83e7b2e578e2caeca4c7
Jorge Malagon <jorge.malagon@roll.mx>
2021-10-20 13:11:05 -0500

    Se ajusta ayuda

--- wp-content/themes/mobig-child/page-templates/page-ayuda.php
+++ wp-content/themes/mobig-child/page-templates/page-ayuda.php
@@ -10,34 +10,62 @@
 ?>
 
   <article class="bg-gen">
-    <section class="hero-sec">
-      <div class="container">
-        <div class="row">
-
-        </div><!-- end.row -->
-      </div><!-- end.container -->
+    <section class="bannerInt">
+      <div class="titleInt">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-10 col-lg-10">
+              <h1>AYUDA</h1>
+              <p>Ofrecemos soluciones en comunicaciones para todos los sectores con tecnología 4.5G</p>
+            </div><!-- end.col-* -->
+          </div><!-- end.row -->
+        </div><!-- end.container -->
+      </div><!-- end.titleInt -->
+      <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/contacto/icono-bg.svg" alt="Icono" class="iconBg">
+      <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/contacto/header-imagen-2.png" alt="Icono" class="imgAbsBan">
     </section><!-- end.hero-sec -->
-    <section class="cobertura-sec">
+    
+    <section class="beneficiosct">
       <div class="container">
-        <div class="row">
-
+        <div class="row justify-content-center">
+          <div class="col-12">
+            <h2 class="beneficiosct__title wow fadeInDown" data-wow-delay="0.6s" data-wow-duration="1s">Los beneficios de estar con moBig</h2>
+          </div><!-- end.col-* -->
+          <div class="col-12 col-md-5 col-lg-3">
+            <figure class="beneficiosct__fig beneficiosct__fig--gray wow fadeIn" data-wow-delay="0.4s" data-wow-duration="1s">
+              <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/home/iconos-04.svg" class="beneficiosct__img" alt="Beneficios-planes">
+              <figcaption class="beneficiosct__cap">
+                <p>Te ofrecemos planes de prepago desde $30, planes de pospago desde $100 y la posibilidad de tener un plan actualizado pagando 7 meses y recibiendo 12.</p>
+              </figcaption><!-- end.beneficiosct__cap -->
+            </figure><!-- end.beneficiosct__fig -->
+          </div><!-- end.col-* -->
+          <div class="col-12 col-md-5 col-lg-3">
+            <figure class="beneficiosct__fig beneficiosct__fig--pink wow fadeIn" data-wow-delay="0.6s" data-wow-duration="1s">
+              <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/home/iconos-04.svg" class="beneficiosct__img" alt="beneficios-linea">
+              <figcaption class="beneficiosct__cap">
+                <p>Porta tu línea y quédate con tu número de toda la vida.</p>
+              </figcaption><!-- end.beneficiosct__cap -->
+            </figure><!-- end.beneficiosct__fig -->
+          </div><!-- end.col-* -->
+          <div class="col-12 col-md-5 col-lg-3">
+            <figure class="beneficiosct__fig beneficiosct__fig--gray wow fadeIn" data-wow-delay="0.8s" data-wow-duration="1s">
+              <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/home/iconos-06.svg" class="beneficiosct__img" alt="beneficios-velocidad">
+              <figcaption class="beneficiosct__cap">
+                <p>Disfruta la mayo velocidad para navegar con la nueva red 4.5G y 5G.</p>
+              </figcaption><!-- end.beneficiosct__cap -->
+            </figure><!-- end.beneficiosct__fig -->
+          </div><!-- end.col-* -->
+          <div class="col-12 col-md-5 col-lg-3">
+            <figure class="beneficiosct__fig beneficiosct__fig--pink wow fadeIn" data-wow-delay="1s" data-wow-duration="1s">
+              <img src="<?php echo get_stylesheet_directory_uri(''); ?>/assets/img/home/iconos-07.svg" class="beneficiosct__img" alt="beneficios-cobertura">
+              <figcaption class="beneficiosct__cap">
+                <p>Mantente conectado en cualquier parte del mundo, con nuestra cobertura nacional e internacional.</p>
+              </figcaption><!-- end.beneficiosct__cap -->
+            </figure><!-- end.beneficiosct__fig -->
+          </div><!-- end.col-* -->
         </div><!-- end.row -->
       </div><!-- end.container -->
-    </section><!-- end.cobertura-sec -->
-    <section class="zonawifi-sec">
-      <div class="container">
-        <div class="row">
-
-        </div><!-- end.row -->
-      </div><!-- end.container -->
-    </section><!-- end.zonawifi-sec -->
-    <section class="beneficios-sec">
-      <div class="container">
-        <div class="row">
-
-        </div><!-- end.row -->
-      </div><!-- end.container -->
-    </section><!-- end.beneficios-sec -->
+    </section><!-- end.beneficiosct -->
   </article><!-- end.article inicio -->
 
 <?php
